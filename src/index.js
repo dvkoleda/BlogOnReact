2e68220eb5f4ae1a38a90b98931f0bfa38d8cd33
Denis <dvkoleda@gmail.com>
2017-06-05 19:44:29 +0300

    Added example of route components.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './components/app';
 import rootReducer from './ducks/index';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
+class Hello extends React.Component {
+  render () {
+    return <div>Hello</div>
+  }
+}
+
+class Goodbay extends React.Component {
+    render () {
+        return <div>Goodbay</div>
+    }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(rootReducer)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello}/>
+        <Route path="/goodbay" component={Goodbay}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
