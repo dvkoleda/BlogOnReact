271e4f2d4e8799037289f4a64f661f253c79f2e5
koleda_d <deniskoleda@iba.by>
2017-06-06 18:35:55 +0300

    Added posts list implementation

--- src/components/post-index.js
+++ src/components/post-index.js
@@ -4,19 +4,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../ducks/post';
+import _ from 'lodash';
 
 class PostIndex extends Component {
     componentDidMount() {
         this.props.fetchPosts();
     }
 
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            )
+        });
+    }
+
     render() {
         return (
             <div>
-                Post index
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-export default connect(null, { fetchPosts })(PostIndex);
+function mapStateToProps(state) {
+    return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
